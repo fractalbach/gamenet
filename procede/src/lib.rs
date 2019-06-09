ec4caa15294cea4cfe12aef8a1e00d975c36b9db
TryExceptElse <schwarzjohnv@gmail.com>
2019-06-09 00:05:45 -0700

    Add skeleton procedural generation package.

---
+++ procede/src/lib.rs
@@ -0,0 +1,37 @@
+type Vec3 = cgmath::Vector3<f64>;
+
+// --------------------------------------------------------------------
+// Traits
+
+
+trait HeightGen {
+    fn height(&self, v: Vec3) -> f64;
+}
+
+trait NormalGen {
+    fn normal(&self, v: Vec3) -> Vec3;
+}
+
+trait Layer {
+    fn height(&self, h0: f64, v: Vec3) -> f64;
+}
+
+
+// --------------------------------------------------------------------
+// Implementations
+
+
+struct World {
+    layers: [Box<Layer>]
+}
+
+
+struct BasicLayer {
+    noise: noise::Perlin
+}
+
+
+// World --------------------------------------------------------------
+
+
+// BasicLayer ---------------------------------------------------------
