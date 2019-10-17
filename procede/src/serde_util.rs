abae195141883240fcfb45d2ad10490e86954eb6
TryExceptElse <schwarzjohnv@gmail.com>
2019-10-16 19:54:04 -0700

    Add SerializableVector3 to serde_util.

--- procede/src/serde_util.rs
+++ procede/src/serde_util.rs
@@ -1,4 +1,4 @@
-use cgmath::Vector2;
+use cgmath::{Vector2, Vector3};
 use serde::{Deserialize, Serialize};
 use serde::Serializer;
 use serde::ser::SerializeStruct;
@@ -10,12 +10,26 @@ pub struct SerializableVector2<T> {
     y: T
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SerializableVector3<T> {
+    x: T,
+    y: T,
+    z: T
+}
+
 impl<T> SerializableVector2<T> {
     pub fn new(v: &Vector2<T>) -> SerializableVector2<T>
         where T: Copy
     {
         SerializableVector2 { x: v.x, y: v.y }
     }
-
-    // TODO serialize()
 }
+
+impl<T> SerializableVector3<T> {
+    pub fn new(v: &Vector3<T>) -> SerializableVector3<T>
+        where T: Copy
+    {
+        SerializableVector3 { x: v.x, y: v.y, z: v.z }
+    }
+}
+
