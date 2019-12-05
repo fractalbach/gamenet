b5c991a02d250d7c932a5555bbf565c7765444e4
TryExceptElse <schwarzjohnv@gmail.com>
2019-12-05 00:04:41 -0800

    Implement StreetMap.find_nearest_node().

--- procede/src/pop/streets.rs
+++ procede/src/pop/streets.rs
@@ -6,7 +6,8 @@
 //!     StreetSegment
 //!     CityRiverSegment
 //!     BuildingSegment
-//! Implement TownMap.add(o: Navigable)
+//! Develop trait-based parameters & callback.
+//! Implement TownMap.add(o: Builder)
 //! Ensure serializable.
 //! Implement village test.
 //! Implement graph visualization.
@@ -56,8 +57,8 @@ struct Node {
 #[derive(Clone, Debug)]
 struct EdgeInfo {
     cost_mod: f64,  // Travel cost of edge. Lower is better.
-    a: u32,
-    b: u32,
+    a: NodeId,
+    b: NodeId,
 }
 
 
@@ -80,10 +81,11 @@ struct ObstacleLine {
 }
 
 
+#[derive(Clone, Debug)]
 pub struct StreetSettings {
-    ideal_edge_len: f64,
-    max_edge_len: f64,
-    min_edge_len: f64
+    base_edge_len: f64,
+    max_edge_len_ratio: f64,
+    min_edge_len_ratio: f64
     // As const settings are required, they should be added here.
 }
 
@@ -91,30 +93,36 @@ pub struct StreetSettings {
 /// Trait for objects which will be added to the city map.
 ///
 /// Allows convenient, logical additions of objects to the city map.
-trait StreetMapObj {
+trait Builder {
     /// Gets nodes which will be added to the city map
     ///
     /// These simply specify points which roads may pass through.
     ///
     /// The passed node positions may may be modified by the StreetMap
     /// to combine passed positions with existing nodes on the map.
-    fn add(&mut self, map: &mut StreetMap) {
+    fn build(&mut self, map: &mut StreetMap) {
         // TODO
     }
 }
 
 
 /// Struct handling logic for a single street segment.
-struct StreetSegment {
+struct StreetSegmentBuilder {
     a: Vector2<f64>,
     b: Vector2<f64>,
     cost: f64,  // Travel cost for this segment. Lower == better.
 }
 
 
-struct CityRiverSegment {
+/// Struct handling creation of river segments within a city map.
+///
+/// This does not add an actual river, only allows one to be integrated
+/// into the city map - allowing roads to follow the banks, bridges to
+/// cross it, and preventing roads from being built through it.
+struct CityRiverSegmentBuilder {
     a: Vector2<f64>,
     b: Vector2<f64>,
+    width: f64,
 }
 
 
@@ -142,7 +150,11 @@ impl StreetMap {
     // Addition methods.
 
     /// Adds passed street map object to the map.
-    pub fn add(&mut self, obj: &StreetMapObj) {
+    pub fn add<I>(&mut self, obj: &mut I)
+    where I: Builder {
+        // This function exists only for convenience, and invokes the
+        // Builder. It may be removed.
+        obj.build(self);
     }
 
     /// Adds a node to the StreetMap.
@@ -168,6 +180,47 @@ impl StreetMap {
 
     fn get_node(&self, id: NodeId) -> Option<&Node> {
         self.nodes.get(id.0)
+    }
+
+    /// Gets node nearest to the passed UV coordinates,
+    /// within the passed radius.
+    fn find_nearest_node(
+        &self, uv: Vector2<f64>, r: f64
+    ) -> Option<(&Node, f64, NodeId)> {
+        let uv_p = vec_to_point(uv);
+        let rect = Rect::centered_with_radius(&uv_p, r as f32);
+
+        // Query Nodes within rect.
+        let query_res = self.nodes.query(rect);
+        if query_res.is_empty() {
+            return Option::None;
+        }
+
+        // Find which result is closest.
+        let first_res = query_res[0].1;
+        debug_assert!(first_res.top_left() == first_res.bottom_right());
+        let mut nearest_d2 = query_res[0].1.top_left().distance_2(&uv_p);
+        let mut nearest_i = 0usize;
+        for i in 1..query_res.len() {
+            let res = query_res[i];
+            let node_rect: &Rect = res.1;
+            debug_assert!(node_rect.top_left() == node_rect.bottom_right());
+            let d2 = node_rect.top_left().distance_2(&uv_p);
+            if d2 < nearest_d2 {
+                nearest_d2 = d2;
+                nearest_i = i;
+            }
+        }
+
+        // Check that distance to nearest node is less than r.
+        // Otherwise, return None.
+        let d = nearest_d2.sqrt() as f64;
+        if d > r {
+            return Option::None;
+        }
+
+        let nearest_res = query_res[nearest_i];
+        Option::Some((nearest_res.0, d, NodeId(nearest_res.2)))
     }
 
     fn get_obstacle_line(&self, id: ObstacleId) -> Option<&ObstacleLine> {
@@ -225,6 +278,7 @@ impl Spatial for Edge {
 // Utility functions
 
 
+// TODO: Simplify w/ Rect member methods.
 /// Finds the bounding box for a Line.
 ///
 /// # Arguments
@@ -235,17 +289,17 @@ impl Spatial for Edge {
 /// Bounding box Rect.
 fn find_line_bounds(a: Vector2<f64>, b: Vector2<f64>) -> Rect {
     macro_rules! min {
-            ($a:expr, $b:expr) => {{
-                let (a, b) = ($a, $b);
-                if a < b { a } else { b }
-            }};
-        };
+        ($a:expr, $b:expr) => {{
+            let (a, b) = ($a, $b);
+            if a < b { a } else { b }
+        }};
+    };
     macro_rules! max {
-            ($a:expr, $b:expr) => {{
-                let (a, b) = ($a, $b);
-                if a < b { b } else { a }
-            }};
-        };
+        ($a:expr, $b:expr) => {{
+            let (a, b) = ($a, $b);
+            if a < b { b } else { a }
+        }};
+    };
 
     let min_x = min!(a.x, b.x);
     let max_x = max!(a.x, b.x);
@@ -261,18 +315,22 @@ fn find_line_bounds(a: Vector2<f64>, b: Vector2<f64>) -> Rect {
     }
 }
 
+fn vec_to_point(v: Vector2<f64>) -> Point {
+    Point { x: v.x as f32, y: v.y as f32 }
+}
+
 
 // Street Map Objects
 
 
-impl StreetSegment {
+impl StreetSegmentBuilder {
     pub fn new(
         a: Vector2<f64>,
         b: Vector2<f64>,
         cost_mod: f64
-    ) -> StreetSegment {
+    ) -> StreetSegmentBuilder {
         let cost = a.distance(b) * cost_mod;
-        StreetSegment { a, b, cost }
+        StreetSegmentBuilder { a, b, cost }
     }
 }
 
@@ -281,7 +339,18 @@ impl StreetSegment {
 mod tests {
     use cgmath::vec2;
 
-    use pop::streets::StreetSegment;
+    use pop::streets::{StreetSegmentBuilder, StreetMap, StreetSettings, Node};
+
+    fn get_default_settings() -> StreetSettings {
+        StreetSettings {
+            base_edge_len: 100.0,
+            max_edge_len_ratio: 1.5,
+            min_edge_len_ratio: 0.5,
+        }
+    }
+
+    // ----------------------------
+    // StreetMap
 
     #[test]
     fn test_simple_initialization() {
@@ -292,8 +361,30 @@ mod tests {
             let v = 0.0;
 
             let street_segment =
-                StreetSegment::new(vec2(u0, v), vec2(u1, v), 1.0);
+                StreetSegmentBuilder::new(vec2(u0, v), vec2(u1, v), 1.0);
         }
         assert!(true);  // Placeholder
     }
+
+    #[test]
+    fn test_find_nearest_node() {
+        let settings = get_default_settings();
+        let mut map = StreetMap::new(settings);
+
+        map.add_node(&Node::new(vec2(0.0, 1000.0)));
+        map.add_node(&Node::new(vec2(0.0, 0.0)));  // Should be nearest.
+        map.add_node(&Node::new(vec2(1000.0, 0.0)));
+        map.add_node(&Node::new(vec2(-500.0, -500.0)));
+        map.add_node(&Node::new(vec2(100.0, -200.0)));
+        map.add_node(&Node::new(vec2(-200.0, 100.0)));
+
+        let (node, d, id) =
+            map.find_nearest_node(vec2(200.0, 200.0), 300.0).unwrap();
+
+        assert_vec2_near!(node.uv, vec2(0.0, 0.0));
+    }
+
+    // ----------------------------
+    // StreetSegment
+
 }
