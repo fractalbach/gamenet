1c82d08a8292c30e5c49cb3108d2d438bd0bfb81
TryExceptElse <schwarzjohnv@gmail.com>
2020-04-10 17:55:01 -0700

    Add adapter function for voronoi().

--- procede/src/util/voronoi.rs
+++ procede/src/util/voronoi.rs
@@ -2,10 +2,18 @@
 use std::iter::FromIterator;
 
 use geo_types::{Polygon, Line, Point, LineString};
-pub use voronoi::{voronoi, DCEL, Point as VoronoiPoint};
+pub use voronoi::{DCEL, Point as VoronoiPoint};
+use voronoi::voronoi as voronoi_inner;
 use voronoi::make_polygons as make_polygons_inner;
 use voronoi::make_line_segments as make_line_segments_inner;
 
+
+pub fn voronoi(points: Vec<Point<f64>>, boxsize: f64) -> DCEL {
+    voronoi_inner(
+        Vec::from_iter(points.iter().map(|point| to_voronoi_point(*point))),
+        boxsize
+    )
+}
 
 pub fn make_polygons(dcel: &DCEL) -> Vec<Polygon<f64>> {
     Vec::from_iter(make_polygons_inner(dcel).iter().map(|points| {
@@ -23,4 +31,8 @@ pub fn make_line_segments(dcel: &DCEL) -> Vec<Line<f64>> {
 
 fn to_point(point: VoronoiPoint) -> geo_types::Point<f64> {
     Point::new(point.x.into(), point.y.into())
+}
+
+fn to_voronoi_point(point: Point<f64>) -> VoronoiPoint {
+    VoronoiPoint::new(point.x().into(), point.y().into())
 }
